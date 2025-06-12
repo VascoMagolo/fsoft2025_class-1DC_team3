2db85a794ee8b847a31047ec25af1c7098c2b28f
Vasco Magolo <vascomagolo@gmail.com>
2025-06-12 08:46:12 +0100

    Add external functions for account number generation and enhance ConsoleView input methods
    Signed-off-by: Vasco Magolo <vascomagolo@gmail.com>

---
+++ Project/external_functions/functions.h
@@ -0,0 +1,12 @@
+//
+// Created by vasco on 12/06/2025.
+//
+
+#ifndef FSOFT2025_CLASS_1DC_TEAM3_FUNCTIONS_H
+#define FSOFT2025_CLASS_1DC_TEAM3_FUNCTIONS_H
+
+
+int generateUniqueAccountNumber(); // Function to generate a unique account number
+
+
+#endif //FSOFT2025_CLASS_1DC_TEAM3_FUNCTIONS_H
