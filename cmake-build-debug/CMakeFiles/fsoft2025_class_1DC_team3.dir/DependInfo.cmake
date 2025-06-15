549c7c2eb599a4004d05a8e73eb9c5fb3a20c61e
Vasco Magolo <vascomagolo@gmail.com>
2025-06-15 18:31:49 +0100

    Update test log timestamps and correct account name in JSON
    Signed-off-by: Vasco Magolo <vascomagolo@gmail.com>

---
+++ cmake-build-debug/CMakeFiles/fsoft2025_class_1DC_team3.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/controllers/AccountController.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AccountController.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AccountController.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/controllers/AdminController.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AdminController.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AdminController.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/controllers/MainController.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/MainController.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/MainController.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/data/AccountRepository.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/data/AccountRepository.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/data/AccountRepository.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/main.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/main.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/main.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/models/BankAccount.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/models/BankAccount.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/models/BankAccount.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/services/PasswordServices.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/services/PasswordServices.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/services/PasswordServices.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/views/ConsoleView.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ConsoleView.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ConsoleView.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/Project/views/ReceiptView.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ReceiptView.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ReceiptView.cpp.obj.d"
+  "C:/Users/vasco/CLionProjects/fsoft2025_class-1DC_team3/ProjectTest/main.cpp" "CMakeFiles/fsoft2025_class_1DC_team3.dir/ProjectTest/main.cpp.obj" "gcc" "CMakeFiles/fsoft2025_class_1DC_team3.dir/ProjectTest/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/fsoft2025_class_1DC_team3.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AccountController.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AccountController.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AdminController.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/AdminController.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/MainController.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/controllers/MainController.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/data/AccountRepository.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/data/AccountRepository.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/main.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/main.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/models/BankAccount.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/models/BankAccount.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/services/PasswordServices.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/services/PasswordServices.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ConsoleView.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ConsoleView.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ReceiptView.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/Project/views/ReceiptView.cpp.obj.d"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/ProjectTest/main.cpp.obj"
+  "CMakeFiles/fsoft2025_class_1DC_team3.dir/ProjectTest/main.cpp.obj.d"
+  "fsoft2025_class_1DC_team3.exe"
+  "fsoft2025_class_1DC_team3.exe.manifest"
+  "fsoft2025_class_1DC_team3.pdb"
+  "libfsoft2025_class_1DC_team3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fsoft2025_class_1DC_team3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
